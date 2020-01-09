4423e43992896cf2eb5ef6a287e7d30f5a3bbb84
kpirogov <k.pirogov@8bitgroup.com>
2020-01-09 20:11:58 +0300

    add conf lesson 2

--- Scripts/Lessons/lesson36_step9/conftest.py
+++ Scripts/Lessons/lesson36_step9/conftest.py
@@ -6,14 +6,25 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
                      help="Choose language: ru, fr, etc.")
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
 
 # фикстура для открытия и закрытия браузера с выбором языка
 @pytest.fixture(scope="function")
 def browser(request):
-    user_language = request.config.getoption("language")
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome" or "":
+        print("\nstart chrome browser for test..")
+        user_language = request.config.getoption("language")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
--- Scripts/Lessons/lesson36_step9/test_items.py
+++ Scripts/Lessons/lesson36_step9/test_items.py
@@ -7,5 +7,5 @@ def test_check_add_to_bucket_button(browser):
     browser.get(link)
     check_message = browser.find_element_by_css_selector("#add_to_basket_form button")
     print("\n", check_message.text)
-    assert "Añadir al carrito" in check_message.text, f"Текст сообщения {check_message.text}"
-    time.sleep(10)
+    assert "Añadir al carrito" in check_message.text, f"Текст сообщения {check_message.text}" #проверяю согласно заданию только испанский
+    time.sleep(10) #слип для проверки французского
